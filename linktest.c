837bd6f07f9ff539f4a124e65d47a84ae8179389
James Wilson <macihenroomfiddling@gmail.com>
2016-08-05 19:46:17 +0100

    implement read() and basic transputer type check

--- linktest.c
+++ linktest.c
@@ -1,23 +1,37 @@
 #include <tsplib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 int main (int argc, char **argv) {
-    int fd;
+    int wfd;
+    int rfd;
     int rc;
-    fd = tsp_open(argv[1]);
-    printf ("tsp_open (%s) = %d\n", argv[1], fd);
-    rc = tsp_reset(fd);
-    printf ("tsp_reset = %d\n", rc);
-    rc = tsp_analyse(fd);
+    wfd = tsp_open(argv[1]);
+    printf ("tsp_open (%s) = %d\n", argv[1], wfd);
+    rfd = tsp_open(argv[2]);
+    printf ("tsp_open (%s) = %d\n", argv[2], rfd);
+    rc = tsp_reset(wfd);
+    printf ("tsp_reset (write) = %d\n", rc);
+    rc = tsp_reset(rfd);
+    printf ("tsp_reset (read)  = %d\n", rc);
+/*    rc = tsp_analyse(wfd);
     printf ("tsp_analyse = %d\n", rc);
-    rc = tsp_error(fd);
-    printf ("tsp_error = %d\n", rc);
-    rc = tsp_protocol(fd, TSP_RAW_PROTOCOL, 4096);
-    printf ("tsp_protocol = %d\n", rc);
-    while (1) {
-        uint8_t data[] = {0,128,255,0,128,255,0,128,255,0};
-        rc = tsp_write( fd, data, 10, 2 );
-        printf ("tsp_write = %d\n", rc);
-    }
+    rc = tsp_error(wfd);
+    printf ("tsp_error = %d\n", rc);*/
+    rc = tsp_protocol(wfd, TSP_RAW_PROTOCOL, 4096);
+    printf ("tsp_protocol (write) = %d\n", rc);
+    rc = tsp_protocol(rfd, TSP_RAW_PROTOCOL, 4096);
+    printf ("tsp_protocol (read)  = %d\n", rc);
+    //blatant copy from https://github.com/hessch/rpilink/blob/master/utils/tdetect.py
+    uint8_t data[] = {0xb1,0xd1,0x24,0xf2,0x21,0xfc,0x24,
+                      0xf2,0x21,0xf8,0xf0,0x60,0x5c,0x2a,
+                      0x2a,0x2a,0x4a,0xff,0x21,0x2f,0xff,
+                      0x02,0x00};
+    rc = tsp_write(wfd, data, sizeof(data), 2 );
+    printf ("tsp_write = %d\n", rc);
+    uint8_t rx[256];
+    memset (rx, 0xFF, sizeof(rx));
+    rc = tsp_read(rfd, rx, 4/*sizeof(rx)*/, 2 );
+    printf ("tsp_read = %d [%02X %02X %02X %02X]\n", rc, rx[0], rx[1], rx[2], rx[3]);
 }
--- tsplib.c
+++ tsplib.c
@@ -20,143 +20,154 @@
 #define DEFAULT_TIMEOUT (5)
 
 static void dump_error (sg_io_hdr_t *io_hdr) {
+    bool status_ok = true;
     bool info_ok = true;
     bool host_ok = false;
     bool driver_ok = false;
-    if (io_hdr->info & 0x01) {
-        printf ("info = SG_INFO_CHECK\n");
-        info_ok = false;
+    if (io_hdr->status != 0) {
+        printf ("status = %02X : ", io_hdr->status);
+        switch (io_hdr->status) {
+            case 0x02: printf ("check condition\n"); break;
+            default: printf ("*unknown*\n"); break;
+        }
+        status_ok = false;
     }
-    if (!info_ok) {
-        switch (io_hdr->host_status) {
-            case 0x0:
-                host_ok = true;
-                printf ("host status = OK\n");
-                break;
-            case 0x1:
-                printf ("host status = SG_ERR_DID_NO_CONNECT\n");
-                break;
-            case 0x2:
-                printf ("host status = SG_ERR_DID_BUS_BUSY\n");
-                break;
-            case 0x3:
-                printf ("host status = SG_ERR_DID_TIME_OUT\n");
-                break;
-            case 0x4:
-                printf ("host status = SG_ERR_DID_BAD_TARGET\n");
-                break;
-            case 0x5:
-                printf ("host status = SG_ERR_DID_ABORT\n");
-                break;
-            case 0x6:
-                printf ("host status = SG_ERR_DID_PARITY\n");
-                break;
-            case 0x7:
-                printf ("host status = SG_ERR_DID_ERROR\n");
-                break;
-            case 0x8:
-                printf ("host status = SG_ERR_DID_RESET\n");
-                break;
-            case 0x9:
-                printf ("host status = SG_ERR_DID_BAD_INTR\n");
-                break;
-            case 0xA:
-                printf ("host status = SG_ERR_DID_PASSTHROUGH\n");
-                break;
-            case 0xB:
-                printf ("host status = SG_ERR_DID_SOFT_ERROR\n");
-                break;
-            default:
-                printf ("unknown host status 0x%X\n", io_hdr->host_status);
-                break;
+    if (status_ok) {
+        if (io_hdr->info & 0x01) {
+            printf ("info = SG_INFO_CHECK\n");
+            info_ok = false;
         }
-        //LS nibble is error, MS nibble is suggestion
-        switch (io_hdr->driver_status & 0x0F) {
-            case 0:
-                driver_ok = true;
-                printf ("driver status = OK\n");
-                break;
-            case 1:
-                printf ("*E* DRIVER_BUSY\n");
-                break;
-            case 2:
-                printf ("*E* DRIVER_SOFT\n");
-                break;
-            case 3:
-                printf ("*E* DRIVER_MEDIA\n");
-                break;
-            case 4:
-                printf ("*E* DRIVER_ERROR\n");
-                break;
-            case 5:
-                printf ("*E* DRIVER_INVALID\n");
-                break;
-            case 6:
-                printf ("*E* DRIVER_TIMEOUT\n");
-                break;
-            case 7:
-                printf ("*E* DRIVER_HARD\n");
-                break;
-            case 8:
-                printf ("*E* DRIVER_SENSE\n");
-                if (io_hdr->sb_len_wr > 0) {
-                    int i;
-                    printf ("sense buffer :");
-                    for (i=0; i < io_hdr->sb_len_wr; i++) {
-                        printf (" %02X", io_hdr->sbp[i]);
-                    }
-                    uint8_t sense_key = io_hdr->sbp[2];
-                    printf ("\nsense key = 0x%X = ", sense_key);
-                    switch (sense_key) {
-                        case 0x00: printf ("NO SENSE"); break;
-                        case 0x01: printf ("RECOVERED ERROR"); break;
-                        case 0x02: printf ("NOT READY"); break;
-                        case 0x03: printf ("MEDIUM ERROR"); break;
-                        case 0x04: printf ("HARDWARE ERROR"); break;
-                        case 0x05: printf ("ILLEGAL REQUEST"); break;
-                        case 0x06: printf ("UNIT ATTENTION"); break;
-                        case 0x07: printf ("DATA PROTECT"); break;
-                        case 0x08: printf ("BLANK CHECK"); break;
-                        case 0x09: printf ("VENDOR SPECIFIC"); break;
-                        case 0x0A: printf ("COPY ABORTED"); break;
-                        case 0x0B: printf ("ABORTED COMMAND"); break;
-                        case 0x0D: printf ("VOLUME OVERFLOW"); break;
-                        case 0x0E: printf ("MISCOMPARE"); break;
-                        case 0x0F: printf ("COMPLETED"); break;
-                        case 0x0C: printf ("**UNKNOWN**"); break;
-                    }
-                    printf ("\n");
-                    if (io_hdr->sbp[7] == 0x0A) {
-                        //additional sense bytes
-                        uint8_t ASC = io_hdr->sbp[12];
-                        uint8_t ASCQ = io_hdr->sbp[13];
-                        printf ("ASC = 0x%02X ASCQ = 0x%02X\n", ASC, ASCQ);
-                    }
-                }
-                break;
-            default:
-                printf ("unknown driver_status %X\n", io_hdr->driver_status);
-                break;
-        }
-        if (!driver_ok) {
-            switch (io_hdr->driver_status & 0xF0 >> 4) {
-                case 1:
-                    printf ("suggest = RETRY\n");
+        if (!info_ok) {
+            switch (io_hdr->host_status) {
+                case 0x0:
+                    host_ok = true;
+                    printf ("host status = OK\n");
                     break;
-                case 2:
-                    printf ("suggest = ABORT\n");
+                case 0x1:
+                    printf ("host status = SG_ERR_DID_NO_CONNECT\n");
                     break;
-                case 3:
-                    printf ("suggest = REMAP\n");
+                case 0x2:
+                    printf ("host status = SG_ERR_DID_BUS_BUSY\n");
                     break;
-                case 4:
-                    printf ("suggest = DIE\n");
+                case 0x3:
+                    printf ("host status = SG_ERR_DID_TIME_OUT\n");
                     break;
-                case 8:
-                    printf ("suggest = SENSE\n");
+                case 0x4:
+                    printf ("host status = SG_ERR_DID_BAD_TARGET\n");
+                    break;
+                case 0x5:
+                    printf ("host status = SG_ERR_DID_ABORT\n");
+                    break;
+                case 0x6:
+                    printf ("host status = SG_ERR_DID_PARITY\n");
+                    break;
+                case 0x7:
+                    printf ("host status = SG_ERR_DID_ERROR\n");
+                    break;
+                case 0x8:
+                    printf ("host status = SG_ERR_DID_RESET\n");
+                    break;
+                case 0x9:
+                    printf ("host status = SG_ERR_DID_BAD_INTR\n");
+                    break;
+                case 0xA:
+                    printf ("host status = SG_ERR_DID_PASSTHROUGH\n");
+                    break;
+                case 0xB:
+                    printf ("host status = SG_ERR_DID_SOFT_ERROR\n");
                     break;
                 default:
-                    printf ("unknown suggestion %X\n", io_hdr->driver_status);
+                    printf ("unknown host status 0x%X\n", io_hdr->host_status);
+                    break;
+            }
+            //LS nibble is error, MS nibble is suggestion
+            switch (io_hdr->driver_status & 0x0F) {
+                case 0:
+                    driver_ok = true;
+                    printf ("driver status = OK\n");
+                    break;
+                case 1:
+                    printf ("*E* DRIVER_BUSY\n");
+                    break;
+                case 2:
+                    printf ("*E* DRIVER_SOFT\n");
+                    break;
+                case 3:
+                    printf ("*E* DRIVER_MEDIA\n");
+                    break;
+                case 4:
+                    printf ("*E* DRIVER_ERROR\n");
+                    break;
+                case 5:
+                    printf ("*E* DRIVER_INVALID\n");
+                    break;
+                case 6:
+                    printf ("*E* DRIVER_TIMEOUT\n");
+                    break;
+                case 7:
+                    printf ("*E* DRIVER_HARD\n");
+                    break;
+                case 8:
+                    printf ("*E* DRIVER_SENSE\n");
+                    if (io_hdr->sb_len_wr > 0) {
+                        int i;
+                        printf ("sense buffer :");
+                        for (i=0; i < io_hdr->sb_len_wr; i++) {
+                            printf (" %02X", io_hdr->sbp[i]);
+                        }
+                        uint8_t sense_key = io_hdr->sbp[2];
+                        printf ("\nsense key = 0x%X = ", sense_key);
+                        switch (sense_key) {
+                            case 0x00: printf ("NO SENSE"); break;
+                            case 0x01: printf ("RECOVERED ERROR"); break;
+                            case 0x02: printf ("NOT READY"); break;
+                            case 0x03: printf ("MEDIUM ERROR"); break;
+                            case 0x04: printf ("HARDWARE ERROR"); break;
+                            case 0x05: printf ("ILLEGAL REQUEST"); break;
+                            case 0x06: printf ("UNIT ATTENTION"); break;
+                            case 0x07: printf ("DATA PROTECT"); break;
+                            case 0x08: printf ("BLANK CHECK"); break;
+                            case 0x09: printf ("VENDOR SPECIFIC"); break;
+                            case 0x0A: printf ("COPY ABORTED"); break;
+                            case 0x0B: printf ("ABORTED COMMAND"); break;
+                            case 0x0D: printf ("VOLUME OVERFLOW"); break;
+                            case 0x0E: printf ("MISCOMPARE"); break;
+                            case 0x0F: printf ("COMPLETED"); break;
+                            case 0x0C: printf ("**UNKNOWN**"); break;
+                        }
+                        printf ("\n");
+                        if (io_hdr->sbp[7] == 0x0A) {
+                            //additional sense bytes
+                            uint8_t ASC = io_hdr->sbp[12];
+                            uint8_t ASCQ = io_hdr->sbp[13];
+                            printf ("ASC = 0x%02X ASCQ = 0x%02X\n", ASC, ASCQ);
+                        }
+                    }
+                    break;
+                default:
+                    printf ("unknown driver_status %X\n", io_hdr->driver_status);
+                    break;
+            }
+            if (!driver_ok) {
+                switch (io_hdr->driver_status & 0xF0 >> 4) {
+                    case 1:
+                        printf ("suggest = RETRY\n");
+                        break;
+                    case 2:
+                        printf ("suggest = ABORT\n");
+                        break;
+                    case 3:
+                        printf ("suggest = REMAP\n");
+                        break;
+                    case 4:
+                        printf ("suggest = DIE\n");
+                        break;
+                    case 8:
+                        printf ("suggest = SENSE\n");
+                        break;
+                    default:
+                        printf ("unknown suggestion %X\n", io_hdr->driver_status);
+                }
             }
         }
     }
@@ -165,9 +176,10 @@ static void dump_error (sg_io_hdr_t *io_hdr) {
 //take syscall (ioctl) sttaus and SG header
 //TSP lib expects calls to return 0 or -1
 int check_error(int syscall_rc, char *scsi_op, sg_io_hdr_t *io_hdr) {
-    if (syscall_rc == 0) {
+    if (syscall_rc == 0 && io_hdr->status == 0) {
         return 0;
     } else {
+        printf ("*E*E*E*E*E*E*E*E*E*E*E*E\n");
         printf ("SCSI op %s failed\n", scsi_op);
         dump_error(io_hdr);
         return -1;
@@ -190,7 +202,7 @@ static int do_command(int fd,
                       char *command_name, 
                       int dir, 
                       uint8_t *buffer, 
-                      unsigned int buffer_size, 
+                      unsigned int *buffer_size, 
                       unsigned int timeout) {
     unsigned char cmdBlk[6];
     unsigned char sense_buffer[32];
@@ -199,9 +211,9 @@ static int do_command(int fd,
     memset(&cmdBlk, 0, sizeof(cmdBlk));
     cmdBlk[0] = command;
     cmdBlk[1] = 0;  //LUN;
-    cmdBlk[2] = (buffer_size >> 16) & 0xFF;
-    cmdBlk[3] = (buffer_size >> 8)  & 0xFF;
-    cmdBlk[4] = (buffer_size >> 0)  & 0xFF;
+    cmdBlk[2] = (*buffer_size >> 16) & 0xFF;
+    cmdBlk[3] = (*buffer_size >> 8)  & 0xFF;
+    cmdBlk[4] = (*buffer_size >> 0)  & 0xFF;
     cmdBlk[5] = 0;  //unused
     /* for verification only*/
 #if 1
@@ -210,24 +222,31 @@ static int do_command(int fd,
 #endif
     memset(&io_hdr, 0, sizeof(io_hdr));
     io_hdr.interface_id = 'S';
+    io_hdr.cmdp = cmdBlk;
     io_hdr.cmd_len = sizeof(cmdBlk);
+    io_hdr.sbp = sense_buffer;
     io_hdr.mx_sb_len = sizeof(sense_buffer);
     io_hdr.dxfer_direction = dir;
-    io_hdr.dxfer_len = buffer_size;
+    io_hdr.dxfer_len = *buffer_size;
     io_hdr.dxferp = buffer;
-    io_hdr.cmdp = cmdBlk;
-    io_hdr.sbp = sense_buffer;
     io_hdr.timeout = timeout * 1000;     /* TSP lib is seconds, sg driver is ms */
     rc = ioctl(fd, SG_IO, &io_hdr);
     ret = check_error (rc, command_name, &io_hdr);
+    /* for verification only*/
+#if 1
+    printf ("%s dxfer_len = %u, resid = %u\n", command_name, io_hdr.dxfer_len, io_hdr.resid);
+#endif
+    *buffer_size = io_hdr.dxfer_len - io_hdr.resid;
     return ret;
 }
 
 int tsp_reset( int fd ) {
     uint8_t buff[5];
     int ret;
+    unsigned int size;
     //read FLAGS
-    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, sizeof(buff), DEFAULT_TIMEOUT);
+    size = sizeof(buff);
+    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, &size, DEFAULT_TIMEOUT);
     if (ret == 0) {
         //write FLAGS
         buff[0] = LFLAG_RESET_LINK;
@@ -238,7 +257,8 @@ int tsp_reset( int fd ) {
          protocol persists until the link is reset.
         */
         buff[1] = TSP_RAW_PROTOCOL;
-        ret = do_command (fd, SCMD_WRITE_FLAGS, "SCMD_WRITE_FLAGS", SG_DXFER_TO_DEV, buff, sizeof(buff), DEFAULT_TIMEOUT);
+        size = sizeof(buff);
+        ret = do_command (fd, SCMD_WRITE_FLAGS, "SCMD_WRITE_FLAGS", SG_DXFER_TO_DEV, buff, &size, DEFAULT_TIMEOUT);
     }
     return ret;
 }
@@ -246,8 +266,10 @@ int tsp_reset( int fd ) {
 int tsp_analyse( int fd ) {
     uint8_t buff[5];
     int ret;
+    unsigned int size;
     //read FLAGS
-    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, sizeof(buff), DEFAULT_TIMEOUT);
+    size = sizeof(buff);
+    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, &size, DEFAULT_TIMEOUT);
     if (ret == 0) {
         //write FLAGS
         buff[0] = LFLAG_SUBSYSTEM_ANALYSE;
@@ -258,7 +280,8 @@ int tsp_analyse( int fd ) {
          protocol persists until the link is reset.
         */
         buff[1] = TSP_RAW_PROTOCOL;
-        ret = do_command (fd, SCMD_WRITE_FLAGS, "SCMD_WRITE_FLAGS", SG_DXFER_TO_DEV, buff, sizeof(buff), DEFAULT_TIMEOUT);
+        size = sizeof(buff);
+        ret = do_command (fd, SCMD_WRITE_FLAGS, "SCMD_WRITE_FLAGS", SG_DXFER_TO_DEV, buff, &size, DEFAULT_TIMEOUT);
     }
     return ret;
 }
@@ -266,8 +289,10 @@ int tsp_analyse( int fd ) {
 int tsp_error( int fd ) {
     uint8_t buff[5];
     int ret;
+    unsigned int size;
     //read FLAGS
-    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, sizeof(buff), DEFAULT_TIMEOUT);
+    size = sizeof(buff);
+    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, &size, DEFAULT_TIMEOUT);
     if (ret == 0) {
         if ((buff[0] & LFLAG_SUBSYSTEM_ERROR) == LFLAG_SUBSYSTEM_ERROR) {
             return 1;
@@ -277,12 +302,24 @@ int tsp_error( int fd ) {
 }
 
 int tsp_read( int fd, void *data, size_t length, int timeout ) {
-    assert(false);
+    int ret;
+    unsigned int size = length;
+    ret = do_command (fd, SCMD_RECEIVE, "SCMD_RECEIVE", SG_DXFER_FROM_DEV, data, &size, timeout);
+    if (ret == 0) {
+        //return bytes read
+        ret = size;
+    }
+    return ret;
 }
 
 int tsp_write( int fd, void *data, size_t length, int timeout ) {
     int ret;
-    ret = do_command (fd, SCMD_SEND, "SCMD_SEND", SG_DXFER_TO_DEV, data, length, timeout);
+    unsigned int size = length;
+    ret = do_command (fd, SCMD_SEND, "SCMD_SEND", SG_DXFER_TO_DEV, data, &size, timeout);
+    if (ret == 0) {
+        //return bytes written
+        ret = size;
+    }
     return ret;
 }
 
@@ -290,14 +327,16 @@ int tsp_protocol( int fd, int protocol, int block_size ) {
     uint8_t buff[5];
     int ret;
     //read FLAGS
-    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, sizeof(buff), DEFAULT_TIMEOUT);
+    unsigned int size = sizeof(buff);
+    ret = do_command (fd, SCMD_READ_FLAGS, "SCMD_READ_FLAGS", SG_DXFER_FROM_DEV, buff, &size, DEFAULT_TIMEOUT);
     if (ret == 0) {
         //write protocol & block size
         buff[1] = protocol;
         buff[2] = (block_size >> 16) & 0xFF;
         buff[3] = (block_size >> 8)  & 0xFF;
         buff[4] = (block_size >> 0)  & 0xFF;
-        ret = do_command (fd, SCMD_WRITE_FLAGS, "SCMD_WRITE_FLAGS", SG_DXFER_TO_DEV, buff, sizeof(buff), DEFAULT_TIMEOUT);
+        size = sizeof(buff);
+        ret = do_command (fd, SCMD_WRITE_FLAGS, "SCMD_WRITE_FLAGS", SG_DXFER_TO_DEV, buff, &size, DEFAULT_TIMEOUT);
     }
     return ret;
 }
